0a454274b975ebdd24a6b42cb7a04e717bbafcbd
dev-cesar <cesar.barbosa@bandtec.com.br>
2022-03-31 16:23:27 -0300

    Adding bootstrap

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,36 @@
 import React from "react";
-import Image from 'next/image';
-import ImgProfile from '../images//perfil_cesar_augusto.png';
-
 function Home() {
     return (
         <>
-            <head className="head_banner_code">
-                <figure className="wrapper_profile_img">
-                    <Image src={ImgProfile} className="rounded-full" alt="Imagem de perfil de cesar augusto" />
-                </figure>
-                <div className="box_txt_banner_code">
-                    <h1 className="name_banner_code">
+            <head className="home-head_banner_code">
+                <div className="home-box_txt_banner_code">
+                    <h1 className="home-name_banner_code" id="nameBunnerCode">
                         Cesar Augusto Alves Barbosa
                     </h1>
-                    <h2 className="title_banner_code">
+                    <h2 className="home-title_banner_code">
                         Desenvolvedor Full Stack Júnior
                     </h2>
-                    <h3 className="sub_title_banner_code">
+                    <h3 className="home-sub_title_banner_code">
                         desenvolvendo soluções criativas e inovadoras
                     </h3>
+                    
                 </div>
             </head>
-            <main className="main_container_index" >
-                <h2 className="main_container_index" >
-                    Content
-                </h2>
+            <main className="home-main_sections">
+                <section className="home-contact_plus">
+                    <h3>
+                        Contato
+                    </h3>
+                </section>
+                <div className="home-content_container_index" >
+                    <h2>
+                       Sobre Mim 
+                    </h2>
+                </div>
             </main>
         </>
     )
 }
+
 
 export default Home;
